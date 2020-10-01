dec1471666c730e7b7e333e003a4440dfcc04c88
namli <namli@mail.ru>
2020-10-01 17:12:11 +0200

    HomeWork 01.10.2020

--- js/my-github.js
+++ js/my-github.js
@@ -1,1 +1,37 @@
 // Write code here to communicate with Github
+
+window.onload = main;
+
+/**
+ * Main function what called whan Window is Loading.
+ *
+ */
+function main() {
+  let reposList = document.getElementById("repos-list");
+  reposList.innerHTML = "";
+  let listOfRepo = getListofRepo("namli");
+  listOfRepo.then(res => {
+    res.forEach(item => {
+      let li = document.createElement("li");
+      li.innerText = item;
+      reposList.appendChild(li);
+    });
+  });
+}
+/**
+ * Get list of user repository Name
+ *
+ * @param {string} user
+ * @return {Array} List on Repo name
+ */
+function getListofRepo(user = "namli") {
+  return fetch("https://api.github.com/users/" + user + "/repos")
+    .then(resp => {
+      return resp.json();
+    })
+    .then(data => {
+      return data.map(item => {
+        return item.name;
+      });
+    });
+}
